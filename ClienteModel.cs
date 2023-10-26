2cd3c80b6661eb14b21eccf86b8df2e1cebbdfb4
NilsaMeza <144973169+NilsaMeza@users.noreply.github.com>
2023-10-25 21:17:11 -0300

    ClienteModel.cs

---
+++ ClienteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Infraestructura.Modelos
+{
+    public class ClienteModel
+    {
+        public int IdCliente { get; set; }
+        public int IdPersona { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public required string Calificacion { get; set; }
+        public required string Estado { get; set; }
+    }
+}
