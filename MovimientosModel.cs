7a615dff4d713541e3a16af63d94ed44658fab3b
NilsaMeza <144973169+NilsaMeza@users.noreply.github.com>
2023-10-25 21:18:42 -0300

    MovimientosModel.cs

---
+++ MovimientosModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Infraestructura.Modelos
+{
+    public class MovimientosModel
+    {
+        public int IdMovimiento { get; set; }
+        public int IdCuenta { get; set; }
+        public DateTime FechaMovimiento { get; set; }
+        public string TipoMovimiento { get; set; }
+        public decimal SaldoAnterior { get; set; }
+        public decimal SaldoActual { get; set; }
+        public decimal MontoMovimiento { get; set; }
+        public decimal CuentaOrigen { get; set; }
+        public decimal CuentaDestino { get; set; }
+        public decimal Canal { get; set; }
+    }
+}
