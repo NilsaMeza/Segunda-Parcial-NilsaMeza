8a8c12aeddeb0feea1f460e3decbacd3d89f44b5
NilsaMeza <144973169+NilsaMeza@users.noreply.github.com>
2023-10-25 21:30:03 -0300

    CuentasService.cs

---
+++ CuentasService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Infraestructura.Datos;
+using Infraestructura.Modelos;
+
+namespace Servicios.ContactosService
+{
+    public class CuentasService
+    {
+        CuentasDatos cuentasDatos;
+
+        public CuentasService(string cadenaConexion)
+        {
+            cuentasDatos = new CuentasDatos(cadenaConexion);
+        }
+
+        public void InsertarCuenta(CuentasModel cuenta)
+        {
+            ValidarDatos(cuenta);
+            cuentasDatos.InsertarCuenta(cuenta);
+        }
+
+        public CuentasModel ObtenerCuenta(int id)
+        {
+            return cuentasDatos.ObtenerCuentaPorId(id);
+        }
+
+        public void ModificarCuenta(CuentasModel cuenta)
+        {
+            ValidarDatos(cuenta);
+            cuentasDatos.ModificarCuenta(cuenta);
+        }
+
+        public void EliminarCuenta(int id)
+        {
+            cuentasDatos.EliminarCuenta(id);
+        }
+
+        private void ValidarDatos(CuentasModel cuenta)
+        {
+            if (cuenta.IdCliente <= 0)
+            {
+                throw new Exception("El campo IdCliente es inválido.");
+            }
+
+            if (string.IsNullOrEmpty(cuenta.NroCuenta))
+            {
+                throw new Exception("El campo NroCuenta es obligatorio y no puede estar vacío.");
+            }
+
+        }
+    }
+}
