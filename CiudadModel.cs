be98c7df62db34696df37495923771921c5a8af5
NilsaMeza <144973169+NilsaMeza@users.noreply.github.com>
2023-10-25 21:16:31 -0300

    CiudadModel.cs

---
+++ CiudadModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infraestructura.Modelos
+{
+    public class CiudadModel
+    {
+        public int idCiu { get; set; }
+        public string Ciudad { get; set; }
+        public string Departamento { get; set; }
+        public string Postal_code { get; set; }
+    }
+}
