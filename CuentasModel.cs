b73b4d44cc17f51692a34490ced575dbfbad8dcd
NilsaMeza <144973169+NilsaMeza@users.noreply.github.com>
2023-10-25 21:17:44 -0300

    CuentasModel.cs

---
+++ CuentasModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Infraestructura.Modelos
+{
+    public class CuentasModel
+    {
+        public int Id_cuenta { get; set; }
+        public int id_cliente { get; set; }
+        public required string NroCuenta { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public required string TipoCuenta { get; set; }
+        public required string Estado { get; set; }
+        public decimal Saldo { get; set; }
+        public required string nro_Contrato { get; set; }
+        public decimal CostoMantenimiento { get; set; }
+        public required string PromedioAcreditacion { get; set; }
+        public required string Moneda { get; set; }
+        public required string EstadoCuenta { get; set; }
+    }
+}
